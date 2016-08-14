8199a4a4109ade3ca97e920a6f966152c471a55d
barrinbar@gmail.com <barrinbar@gmail.com>
2016-08-14 19:47:06 +0300

    Added some functionality for showing and hiding level selection

--- app/src/main/java/com/example/inbar/memgame/Loby.java
+++ app/src/main/java/com/example/inbar/memgame/Loby.java
@@ -2,7 +2,12 @@ package com.example.inbar.memgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.log;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Loby extends AppCompatActivity {
 
@@ -16,7 +21,64 @@ public class Loby extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+        hideLevels();
+
+        checkNameFill();
+
         // Load current state?
+    }
+
+    private void hideLevels() {
+        // Hide level selection label
+        TextView tv_level_select = (TextView)findViewById(R.id.tv_select_level);
+        tv_level_select.setVisibility(View.GONE);
+
+        // Disable buttons
+        Button btn_lvl_1 = (Button)findViewById(R.id.btn_lvl_1);
+        Button btn_lvl_2 = (Button)findViewById(R.id.btn_lvl_1);
+        Button btn_lvl_3 = (Button)findViewById(R.id.btn_lvl_1);
+
+        btn_lvl_1.setEnabled(false);
+        btn_lvl_2.setEnabled(false);
+        btn_lvl_3.setEnabled(false);
+    }
+
+    private boolean checkNameFill() {
+        EditText et_name = (EditText)findViewById(R.id.et_name);
+        et_name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() >= 3)
+                    showLevels();
+                else
+                    hideLevels();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    private void showLevels() {
+        // Show level selection label
+        TextView tv_level_select = (TextView)findViewById(R.id.tv_select_level);
+        tv_level_select.setVisibility(View.VISIBLE);
+
+        // Enable buttons
+        Button btn_lvl_1 = (Button)findViewById(R.id.btn_lvl_1);
+        Button btn_lvl_2 = (Button)findViewById(R.id.btn_lvl_1);
+        Button btn_lvl_3 = (Button)findViewById(R.id.btn_lvl_1);
+
+        btn_lvl_1.setEnabled(true);
+        btn_lvl_2.setEnabled(true);
+        btn_lvl_3.setEnabled(true);
     }
 
     @Override
