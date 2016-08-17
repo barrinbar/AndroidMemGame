09621a12dd71bbc1d6e80ff2fcbcb5b2ff293db3
barrinbar@gmail.com <barrinbar@gmail.com>
2016-08-18 00:58:42 +0300

    Level 1 working properly

--- app/src/main/java/com/example/inbar/memgame/Level1.java
+++ app/src/main/java/com/example/inbar/memgame/Level1.java
@@ -3,6 +3,7 @@ package com.example.inbar.memgame;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,38 +17,39 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class level1 extends AppCompatActivity {
 
     private final static int TOTAL_CARDS = 4;
+    private final static int MATCHED = -1;
     private static int numOfMatches = 0;
     private static int numOfFlips = 0;
-    private static ImageButton firstCard;
+    private static ImageView firstCard = null;
+    private static ImageView secondCard = null;
+    private ArrayList<Integer> cards;
 
-    Chronometer curr_time;
-    TextView user_name;
-
+    private Chronometer currTime;
+    private TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
 
-        curr_time = (Chronometer)findViewById(R.id.ch_time);
-        user_name = (TextView)findViewById(R.id.tv_user_name);
-        ArrayList<Integer> cards;
+        currTime = (Chronometer)findViewById(R.id.ch_time);
+        userName = (TextView)findViewById(R.id.tv_user_name);
 
         // Retrieve user name from shared pref
         SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
-        user_name.setText(sharedPref.getString(getString(R.string.saved_user_name), null));
+        userName.setText(sharedPref.getString(getString(R.string.saved_user_name), null));
 
         // Init cards
-        cards = new ArrayList<>();
-        cards.add(R.drawable.begin);
-        cards.add(R.drawable.begin);
-        cards.add(R.drawable.ben_gurion);
-        cards.add(R.drawable.ben_gurion);
+        cards = new ArrayList<>(Arrays.asList(
+                R.drawable.begin, R.drawable.ben_gurion,
+                R.drawable.begin, R.drawable.ben_gurion));
+
         Collections.shuffle(cards);
 
         GridView board = (GridView)findViewById(R.id.board);
@@ -57,65 +59,67 @@ public class level1 extends AppCompatActivity {
         board.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-        /*        Toast.makeText(level1.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        View.OnClickListener imageClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {*/
-                numOfFlips++;
 
-                // TODO: flip curr card and disable clickable on image
-                if (numOfFlips % 2 == 0) {
-                    // get first card and init
-                    firstCard = (ImageButton)v;
-                    firstCard.setImageResource((int)firstCard.getTag());
-                    firstCard.setClickable(false);
+            // Make sure card isn't already matched
+            if (cards.get(v.getId()) == MATCHED) {
+                Toast.makeText(level1.this, "Clicked " + v.getTag(), Toast.LENGTH_SHORT).show();
+                return;
+            }
+            numOfFlips++;
+
+            // If it's a first flip
+            if (firstCard == null) {
+
+                // Get first card and flip it
+                firstCard = (ImageView)v;
+                firstCard.setImageResource((int)firstCard.getTag());
+            }
+            else { // Second flip
+                secondCard = (ImageView)v;
+
+                // Check if user pressed the same card
+                if (firstCard.getId() == secondCard.getId()) {
+                    firstCard.setImageResource(R.drawable.square);
+                    endTurn();
                 }
                 else {
-                    ImageButton secondCard = (ImageButton)v;
-                    secondCard.setImageResource((int)v.getTag());
-                    firstCard.setClickable(false);
+                    // Flip card
+                    secondCard.setImageResource((int) secondCard.getTag());
 
-                    // Halt
-                    try {
-                        wait(3000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    // Compare to flipped card
-                    if (firstCard.getTag() == secondCard.getTag())
+                    // Check if cards match
+                    if ((int) firstCard.getTag() == (int) secondCard.getTag()) {
+                        // Mark match
                         numOfMatches++;
-                    else {
-                        // Flip back
-                        firstCard.setImageResource(R.drawable.square);
-                        firstCard.setClickable(true);
-                        secondCard.setImageResource(R.drawable.square);
-                        secondCard.setClickable(true);
+                        cards.set(firstCard.getId(), MATCHED);
+                        cards.set(secondCard.getId(), MATCHED);
+                        endTurn();
+                    } else { // In case they don't match
+                        // wait 0.5 sec
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Flip back
+                                firstCard.setImageResource(R.drawable.square);
+                                secondCard.setImageResource(R.drawable.square);
+                                endTurn();
+                            }
+                        }, 1000);
 
                         // Inform the user
                         Toast.makeText(level1.this, "Oops, the cards don't match", Toast.LENGTH_SHORT).show();
                     }
-
-                    // Check the state of the entire game
-                    gameState();
                 }
             }
+            }
         });
-/*
-        // Place cards
-        for (int card: cards) {
-            ImageButton btnCard = new ImageButton(this);
-            btnCard.setImageResource(R.drawable.square);
-            btnCard.setClickable(true);
-            btnCard.setOnClickListener(imageClickListener);
-            btnCard.setBackgroundColor(Color.TRANSPARENT);
-            btnCard.setTag(card);
-            btnCard.setId(card);
-            board.addView(btnCard);
-        }*/
+    }
+    private void endTurn() {
+        // Refresh cards
+        firstCard = null;
+        secondCard = null;
+
+        // Check the state of the entire game
+        gameState();
     }
 
     public class ImageAdapter extends BaseAdapter {
@@ -126,7 +130,7 @@ public class level1 extends AppCompatActivity {
         }
 
         public int getCount() {
-            return mThumbIds.length;
+            return cards.size();
         }
 
         public Object getItem(int position) {
@@ -137,29 +141,24 @@ public class level1 extends AppCompatActivity {
             return 0;
         }
 
-        // create a new ImageView for each item referenced by the Adapter
+        // Create a new ImageView for each item referenced by the Adapter
         public View getView(int position, View convertView, ViewGroup parent) {
             ImageView imageView;
             if (convertView == null) {
                 // if it's not recycled, initialize some attributes
                 imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 imageView.setPadding(8, 8, 8, 8);
             } else {
                 imageView = (ImageView) convertView;
             }
 
-            imageView.setImageResource(mThumbIds[position]);
-            imageView.setTag(mThumbIds[position]);
+            imageView.setImageResource(R.drawable.square);
+            imageView.setTag(cards.get(position));
+            imageView.setId(position);
             return imageView;
         }
-
-        // references to our images
-        private Integer[] mThumbIds = {
-                R.drawable.begin, R.drawable.ben_gurion,
-                R.drawable.begin, R.drawable.ben_gurion
-        };
     }
 
     @Override
@@ -167,16 +166,16 @@ public class level1 extends AppCompatActivity {
         super.onStart();
         numOfMatches = 0;
         numOfFlips = 0;
-        curr_time.start();
+        currTime.start();
     }
 
     private void gameState() {
         if (numOfMatches * 2 == TOTAL_CARDS) {
-            curr_time.stop();
+            currTime.stop();
             Toast.makeText(level1.this, "Congratulations!!! You Won!!!\n" +
-                    "You needed only " + numOfFlips + " turns", Toast.LENGTH_LONG).show();
+                    "You needed only " + numOfFlips + " flips", Toast.LENGTH_LONG).show();
 
-            // Pass game info to HiScore
+            // TODO: Pass game info to HiScore
         }
     }
 }
